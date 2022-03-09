374cf3f9c3092f913eda15bb06754fa922f4bccf
StounhandJ <rmarinicev@gmail.com>
2022-03-09 16:03:10 +0300

    init

---
+++ docker/scripts/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sleep 10
+php artisan migrate
+php artisan db:seed
+php artisan storage:link
+chown www-data:www-data -R /var/www/storage/app
+if (("$PRODUCT" = True)); then
+    php artisan optimize
+fi
